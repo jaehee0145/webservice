99457726fab476bb6d110b9916d245122fe2b07a
Jaehee <jaeheepark0145@gmail.com>
2021-09-06 23:10:33 +0900

    HelloController 생성

---
+++ src/test/java/com/summer/webservice/sprigboot/web/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package com.summer.webservice.sprigboot.web;
+
+import com.summer.webservice.springboot.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(MockMvcRequestBuilders.get("hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
