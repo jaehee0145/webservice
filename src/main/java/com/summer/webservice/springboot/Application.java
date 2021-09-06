56634a2f3cd32980771878d9c3a249373cde544e
Jaehee <jaeheepark0145@gmail.com>
2021-09-06 23:10:13 +0900

    Application.java 메인 클래스 생성

---
+++ src/main/java/com/summer/webservice/springboot/Application.java
@@ -0,0 +1,12 @@
+package com.summer.webservice.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 앞으로 만들 프로젝트의 메인 클래스가 된다.
+@SpringBootApplication  // 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
