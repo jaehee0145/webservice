50dc94880a1aed46c527b8a449ef7b727d192eb3
jaehee <jaeheepark0145@gmail.com>
2021-09-12 12:35:53 +0900

    게시글 전체 조회 추가

--- src/main/java/com/summer/webservice/springboot/service/posts/PostsService.java
+++ src/main/java/com/summer/webservice/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.summer.webservice.springboot.service.posts;
 
 import com.summer.webservice.springboot.domain.posts.Posts;
 import com.summer.webservice.springboot.domain.posts.PostsRepository;
+import com.summer.webservice.springboot.web.dto.PostsListResponseDto;
 import com.summer.webservice.springboot.web.dto.PostsResponseDto;
 import com.summer.webservice.springboot.web.dto.PostsSaveRequestDto;
 import com.summer.webservice.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -33,5 +37,13 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // 트랜잭션 범위는 유지하고 조회기능만 남겨서 조회 속도 개선
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/summer/webservice/springboot/web/IndexController.java
+++ src/main/java/com/summer/webservice/springboot/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.summer.webservice.springboot.web;
 
+import com.summer.webservice.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
         // 머스타치 스타터 덕분에 경로와 파일 확장자는 자동으로 지정된다.
         // src/main/resources/templates/index.mustache 로 전환되어 View Resolver가 처리
