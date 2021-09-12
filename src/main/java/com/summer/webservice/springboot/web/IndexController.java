09d46a0f280029e92e9c9480178ac8d3be3da140
jaehee <jaeheepark0145@gmail.com>
2021-09-12 10:53:24 +0900

    메인페이지 추가

---
+++ src/main/java/com/summer/webservice/springboot/web/IndexController.java
@@ -0,0 +1,16 @@
+package com.summer.webservice.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+        // 머스타치 스타터 덕분에 경로와 파일 확장자는 자동으로 지정된다.
+        // src/main/resources/templates/index.musgtache 로 전환되어 View Resolver가 처리
+        // ViewResolver는 URL 요청의 결과를 전달할 타입과 값을 지정하는 관리자
+    }
+}
