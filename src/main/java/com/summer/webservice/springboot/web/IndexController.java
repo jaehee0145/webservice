6033f7e7da3aa5f27704190a3792bfb0975b19c5
jaehee <jaeheepark0145@gmail.com>
2021-09-12 12:46:31 +0900

    게시글 수정 삭제 기능 추가

--- src/main/java/com/summer/webservice/springboot/web/IndexController.java
+++ src/main/java/com/summer/webservice/springboot/web/IndexController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -24,5 +25,11 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        model.addAttribute("post", postsService.findById(id));
+        return "posts-update";
     }
 }
