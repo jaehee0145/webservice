30816b95d5c3e2ca8fca2edb941ef350c07a2ba6
jaehee <jaeheepark0145@gmail.com>
2021-10-03 22:04:14 +0900

    JUnit, JPA 수정

--- src/test/java/com/summer/webservice/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/summer/webservice/springboot/web/PostsApiControllerTest.java
@@ -5,10 +5,10 @@ import com.summer.webservice.springboot.domain.posts.Posts;
 import com.summer.webservice.springboot.domain.posts.PostsRepository;
 import com.summer.webservice.springboot.web.dto.PostsSaveRequestDto;
 import com.summer.webservice.springboot.web.dto.PostsUpdateRequestDto;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -16,7 +16,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -30,7 +30,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PostsApiControllerTest {
 
@@ -48,14 +48,14 @@ public class PostsApiControllerTest {
 
     private MockMvc mvc;
 
-    @Before
+    @BeforeEach
     public void setup() {
         mvc = MockMvcBuilders.webAppContextSetup(context)
                 .apply(springSecurity())
                 .build();
     }
 
-    @After
+    @AfterEach
     public void tearDown() throws Exception {
         postsRepository.deleteAll();
     }
--- src/test/java/com/summer/webservice/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/summer/webservice/springboot/web/dto/HelloResponseDtoTest.java
@@ -1,6 +1,6 @@
 package com.summer.webservice.springboot.web.dto;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
